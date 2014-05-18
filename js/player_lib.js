35a9fdf6ee1ecf94e4e439c322af64841da28609
Joel Takvorian from pi <joel.takvorian@qaraywa.net>
2014-05-18 20:21:44 +0200

    Revert "Updated player_lib.js for play\pause"
    This reverts commit d1048cdac2da6514586e1aec677de838cb58ff1a.

--- js/player_lib.js
+++ js/player_lib.js
@@ -353,12 +353,12 @@ function updateGUI(json){
 function refreshState(state) {
     if (state == 'play') {
         $('#play').addClass('btn-primary');
-        $('#play i').removeClass('icon-play').addClass('icon-pause');
+        $('#play i').removeClass('icon-pause').addClass('icon-play');
         $('#stop').removeClass('btn-primary');
     } else if (state == 'pause') {
         $('#playlist-position').html('Not playing');
         $('#play').addClass('btn-primary');
-        $('#play i').removeClass('icon-pause').addClass('icon-play');
+        $('#play i').removeClass('icon-play').addClass('icon-pause');
         $('#stop').removeClass('btn-primary');
     } else if (state == 'stop') {
         $('#play').removeClass('btn-primary');
