fe241d7bb7d231f232f2df059e9da22b9bcd720e
Joel Takvorian from pi <joel.takvorian@qaraywa.net>
2014-05-29 08:56:19 +0100

    Fixed chained MPD commands
    (had to read response.. no sleep needed!)

--- inc/player_lib.php
+++ inc/player_lib.php
@@ -44,8 +44,8 @@ $response = readMpdResponse($sock);
 } //end openMpdSocket()
 
 function closeMpdSocket($sock) {
-sendMpdCommand($sock,"close");
-fclose($sock);
+	sendMpdCommand($sock,"close");
+	fclose($sock);
 }
 
 // v2
@@ -59,19 +59,9 @@ function sendMpdCommand($sock,$cmd) {
 	}
 }
 
-function chainMpdCommands($sock, $commands) {
-	foreach ($commands as $command) {
-		fputs($sock, $command."\n");
-		fflush($sock);
-		// MPD seems to be disoriented when it receives several commands chained. Need to sleep a little bit
-		// 200 ms
-		usleep(200000);
-	}
-}
-
 // v3
 function readMpdResponse($sock) {
-$output = "";
+	$output = "";
 			while(!feof($sock)) {
 				$response =  fgets($sock,1024);
 				$output .= $response;
@@ -83,7 +73,14 @@ $output = "";
 					break;
 					}
 			}
-return $output;
+	return $output;
+}
+
+function chainMpdCommands($sock, $commands) {
+        foreach ($commands as $command) {
+                sendMpdCommand($sock, $command);
+                readMpdResponse($sock);
+        }
 }
 
 function loadAllLib($sock) {
@@ -144,21 +141,14 @@ function _organizeJsonLib($flat) {
 }
 
 function playAll($sock, $json) {
-	if (count($json) > 0) {
-		// Clear, add first file and play
-	        $commands = array();
-		array_push($commands, "clear");
-		array_push($commands, "add \"".html_entity_decode($json[0]['file'])."\"");
-		array_push($commands, "play");
-	        chainMpdCommands($sock, $commands);
-
-		// Then add remaining
-		$commands = array();
-		for ($i = 1; $i < count($json); $i++) {
-	                array_push($commands, "add \"".html_entity_decode($json[$i]['file'])."\"");
-	        }
-	        chainMpdCommands($sock, $commands);
+        $commands = array();
+	array_push($commands, "clear");
+	foreach ($json as $song) {
+		$path = $song["file"];
+		array_push($commands, "add \"".html_entity_decode($path)."\"");
 	}
+	array_push($commands, "play");
+	chainMpdCommands($sock, $commands);
 }
 
 function enqueueAll($sock, $json) {
