2638078ea844193d69f0397c2d9632808294d214
root <root@localhost>
2014-08-26 23:19:46 +0000

    Added Nice Startup Sound

--- command/player_wrk.php
+++ command/player_wrk.php
@@ -322,6 +322,11 @@ if (isset($_SESSION['upnpmpdcli']) && $_SESSION['upnpmpdcli'] == 1) {
 	$cmd = '/usr/bin/upmpdcli -f "'.$hostname.'" -l 0 > /dev/null 2>&1 &';
 	sysCmd($cmd);
 } 
+
+//Startup Sound
+$cmd = 'mpg123 -a hw:'.$device.' /var/www/inc/Sounds/startup.mp3 > /dev/null 2>&1 &';
+sysCmd($cmd);
+
 // --- NORMAL STARTUP --- //
 
 // --- WORKER MAIN LOOP --- //
