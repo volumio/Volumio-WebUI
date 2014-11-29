e62aa21fa081a65c104500fd8799591c3b64a6e6
Volumio <info@volumio.org>
2014-11-29 21:04:45 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -591,7 +591,7 @@ if (isset($_POST['spotify']) && $_POST['spotify'] != $_SESSION['spotify']){
 	$content .= "spotify_password = ".$_SESSION['spotpassword']."\n"; 
 	$content .= "audio_output = sox"."\n"; 
 	$content .= "[sox]"."\n"; 
-	$content .= "output_name =  hw:".$device.""."\n";
+	$content .= "output_name =  plughw:".$device.""."\n";
 	if ($_POST['spotifybitrate'] == 0) {
 	$content .= "high_bitrate = false"."\n";
 	}
