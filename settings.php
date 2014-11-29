ae30a8f6474d35d435cdbeeee018d7b10462ec37
Volumio <info@volumio.org>
2014-11-29 20:52:36 +0100

    Using SOX for handling SPOP Output
    Using SOX for handling SPOP Output, this allows output device to be changed

--- settings.php
+++ settings.php
@@ -589,7 +589,8 @@ if (isset($_POST['spotify']) && $_POST['spotify'] != $_SESSION['spotify']){
 	$content .= "[spop]"."\n";
 	$content .= "spotify_username = ".$_SESSION['spotusername']."\n";
 	$content .= "spotify_password = ".$_SESSION['spotpassword']."\n"; 
-	$content .= "audio_output = ao"."\n"; 
+	$content .= "audio_output = sox"."\n"; 
+	$content .= "[sox]"."\n"; 
 	$content .= "output_name =  hw:".$device.""."\n";
 	if ($_POST['spotifybitrate'] == 0) {
 	$content .= "high_bitrate = false"."\n";
