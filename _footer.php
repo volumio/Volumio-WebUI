411c2ca64aa861a977980c5ccb35c181ef7b8938
ning-yu <ningyu@lightflo.ws>
2015-05-23 21:13:53 +0100

    Fix Accidentally Deleted Line
    - Fix deletion of scrollTo include, from commit eb447679d7a87e7c7ff05887b0ab7d0beed7815b

--- _footer.php
+++ _footer.php
@@ -61,6 +61,7 @@
 <script src="js/bootstrap-select.min.js"></script>
 <script src="js/notify.js"></script>
 <script src="js/jquery.countdown.js"></script>
+<script src="js/jquery.scrollTo.min.js"></script>
 <script src="js/volumio.api.js"></script>
 <script src="js/volumio.lazyloader.js"></script>
 <script src="js/volumio.library.js"></script>
